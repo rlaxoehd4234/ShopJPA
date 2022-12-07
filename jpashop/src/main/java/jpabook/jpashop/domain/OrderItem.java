8e857acaa81c5f2774b3a90b58517979321ca26e
kimtaedong <rlaxoehd4234@naver.com>
2022-12-07 22:00:43 +0900

    세부내용 수정

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -17,11 +17,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)  // 모든 로딩은 지연 로딩으로 구현해야 한다.
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)     // 모든 로딩은 지연 로딩으로 구현해야 한다.
     @JoinColumn(name = "order_id")
     private Order order;
     
