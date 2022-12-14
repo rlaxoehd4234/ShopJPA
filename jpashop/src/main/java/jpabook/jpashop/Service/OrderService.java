8339c376ae5400ae71c440fc61df4ab244851805
kimtaedong <rlaxoehd4234@naver.com>
2022-12-14 09:26:51 +0900

    OrderService 개발

--- jpashop/src/main/java/jpabook/jpashop/Service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/Service/OrderService.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.Service;
 
 import jpabook.jpashop.domain.Delivery;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repsository.ItemRepository;
 import jpabook.jpashop.repsository.MemberRepository;
@@ -17,7 +19,6 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
-
     private final ItemRepository itemRepository;
 
     /**
@@ -25,11 +26,33 @@ public class OrderService {
      */
     @Transactional
     public Long order(Long memberId, Long itemId, int count){
-
+        //엔티티 조회
         Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
-
+        //배송정보 생성
         Delivery delivery = new Delivery();
         delivery.setAddress(member.getAddress());
+
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item,item.getPrice(),count);
+        //주문 생성
+        Order order = Order.createOrder(member, delivery,orderItem);
+        //주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
     }
+
+    //취소
+
+    @Transactional
+    public void cancelOrder(Long orderId){
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();;
+    }
+    //검색
+
+    
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -12,6 +14,7 @@ import java.util.List;
 @Setter
 @Getter
 @Table(name ="orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
 
