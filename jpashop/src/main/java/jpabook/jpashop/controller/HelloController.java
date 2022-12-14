b98adc6019fda8206b96a371a18d8ed96d02c6f5
kimtaedong <rlaxoehd4234@naver.com>
2022-12-14 11:31:49 +0900

    MainScreenDev

--- jpashop/src/main/java/jpabook/jpashop/controller/HelloController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop;
+package jpabook.jpashop.controller;
 
 
 import org.springframework.stereotype.Controller;
--- jpashop/src/main/java/jpabook/jpashop/controller/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/JpashopApplication.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop;
+package jpabook.jpashop.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
